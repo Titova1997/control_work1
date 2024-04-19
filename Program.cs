9c15fcb04541354de17293395c0d8e481ae7ba06
Titova1997 <152087142+Titova1997@users.noreply.github.com>
2024-04-20 00:21:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿//Берётся первый массив
+string[] array = { "Hello", "2", "world", ":-)" };
+int i = 0;
+int j = 0;
+Console.WriteLine("Имеющийся массив:"); //для наглядности
+while (i < array.Length)
+{
+    Console.WriteLine(array[i]);
+    i++;
+}
+i = 0;
+
+//Считается количество значений в массиве, удовлетворяющих условию <= 3 символа
+while (i < array.Length)
+{
+    if (array[i].Length <= 3)
+    {
+        j++;
+    }
+    i++;
+}
+i = 0;
+Console.WriteLine();
+
+
+//Cоздаётся массив для переноса значений
+string[] arraySort = new string[j];
+j = 0;
+while (i < array.Length)
+{
+    if (array[i].Length <= 3)
+    {
+        arraySort[j] = array[i];
+        j++;
+    }
+    i++;
+}
+j = 0;
+
+//Выводится отсортированный массив
+Console.WriteLine($"Удовлетворяющие условию значения в массиве: ");
+while (j < arraySort.Length)
+{
+    Console.WriteLine(arraySort[j]);
+    j++;
+}
